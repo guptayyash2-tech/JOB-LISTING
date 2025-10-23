4c16829abf69a7c774934a0ea0b441a4727d8fb2
Yash Gupta <guptayyash2@gmail.com>
2025-10-23 09:21:28 +0530

    Update comapnycontroller.js

--- Backened/Controller/comapnycontroller.js
+++ Backened/Controller/comapnycontroller.js
@@ -1,13 +1,27 @@
-const companyinformation = require("../mongo/adminlogin/company");
 
-
-const createCompanyInformation = async (data) => {
+const createCompanyInformation = async (req, res) => {
   try {
-    const { companyname, companyEmail, companyPhone, website, address, city, pincode, contactPersonName, contactPersonEmail, contactPersonPhone } = req.body;
-    let companyinfo = await companyinformation.findOne({ admin: req.admin._id });
+    const {
+      companyname,
+      companyEmail,
+      companyPhone,
+      website,
+      address,
+      city,
+      pincode,
+      contactPersonName,
+      contactPersonEmail,
+      contactPersonPhone
+    } = req.body;
+
+    // Check if company info already exists for this admin
+    let existingCompany = await companyinformation.findOne({ admin: req.admin._id });
+    if (existingCompany) {
+      return res.status(400).json({ message: "Company information already exists" });
+    }
 
     const companyInfo = new companyinformation({
-      admin: companyinfo._id,
+      admin: req.admin._id,
       companyName: companyname,
       companyEmail,
       companyPhone,
@@ -16,33 +30,15 @@ const createCompanyInformation = async (data) => {
       city,
       pincode,
       contactPersonName,
-      contactPersonEmail, 
+      contactPersonEmail,
       contactPersonPhone
     });
 
     await companyInfo.save();
-    return companyInfo;
+    res.status(201).json({ company: companyInfo });
+
   } catch (error) {
-    throw new Error("Error creating company information");
+    console.error(error);
+    res.status(500).json({ message: "Error creating company information" });
   }
 };
-
-// Get Company Information by User ID
-const getCompanyInformationByUserId = async (req, res) => {
-  try {
-    const userInfo = await companyinformation.findOne({ admin: req.admin._id }).populate(
-      "Admin",
-      "pancard officeemailid mobilenumber"
-    );
-
-    if (!userInfo) {
-      return res.status(404).json({ message: "Personal info not found" });
-    }
-
-    // Wrap in 'user' key for frontend consistency
-    res.status(200).json({ user: userInfo });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-module.exports = { createCompanyInformation, getCompanyInformationByUserId };
